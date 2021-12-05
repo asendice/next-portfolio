3ac5ce368c3ef64bd4d091f40a0370855f5af95d
Dylan Travis <dylan.travis23@outlook.com>
2021-12-04 23:37:20 -0800

    project-item ui

--- components/projects/project-item.js
+++ components/projects/project-item.js
@@ -11,28 +11,46 @@ function ProjectItem(props) {
   };
 
   return (
-    <li className={classes.container}>
-      <Link href={`/projects/${name}`}>
-        <a className={classes.item}>
-          <div>
-            <Image loader={myLoader} width={350} src={images[0]} height={250} />
-          </div>
-          <div className={classes.text}>
-            <h1>{name}</h1>
-            <ul>
-              {skills.map((skill) => (
-                <li key={skill}>{skill}</li>
-              ))}
-            </ul>
-          </div>
-          <div className={classes.btns}>
-            <Button link={gitHub}>GitHub</Button>
-            {live && <Button link={live}>Live Demo</Button>}
-          </div>
-        </a>
-      </Link>
+    <li className={classes.item}>
+      <div className={classes.left}>
+        <div className={classes.image}>
+          <Image loader={myLoader} width={350} src={images[0]} height={220} />
+        </div>
+        <div className={classes.text}>
+          <h1>{name}</h1>
+          <p>{description}</p>
+        </div>
+      </div>
+      <div className={classes.btns}>
+        <Button link={gitHub}>GitHub</Button>
+        {live && <Button link={live}>Live Demo</Button>}
+      </div>
     </li>
   );
+
+  // return (
+  //   <li className={classes.item}>
+  //     <Link href={`/projects/${name}`}>
+  //       <a>
+  //         <div>
+  //           <Image loader={myLoader} width={350} src={images[0]} height={250} />
+  //           <h1>{name}</h1>
+  //         </div>
+  //         <div className={classes.text}>
+  //           <ul>
+  //             {skills.map((skill) => (
+  //               <li key={skill}>{skill}</li>
+  //             ))}
+  //           </ul>
+  //         </div>
+  //       </a>
+  //     </Link>
+  //     <div className={classes.btns}>
+  //       <Button link={gitHub}>GitHub</Button>
+  //       {live && <Button link={live}>Live Demo</Button>}
+  //     </div>
+  //   </li>
+  // );
 }
 
 export default ProjectItem;
