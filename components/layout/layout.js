c4042ac497595248d1f74ceb312d56eaaad3b028
Dylan Travis <dylan.travis23@outlook.com>
2021-12-07 12:36:43 -0800

    made main nav conditional

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,10 +1,15 @@
 import { Fragment } from "react";
+import { useRouter } from "next/router";
 import MainNavigation from "./main-navigation";
 
 function Layout(props) {
+
+  const router = useRouter();
+  const showHeader = router.pathname === "/" ? false : true;
+
   return (
     <Fragment>
-      <MainNavigation />
+      {showHeader && <MainNavigation />}
       {props.children}
     </Fragment>
   )
